9da1309e53741eda213a43ad20280e7fe17dd6a4
Baaber <tatykulov@gmail.com>
2023-06-23 14:48:07 -0500

    this is merged problem

--- src/main/java/org/example/BranchRebase.java
+++ src/main/java/org/example/BranchRebase.java
@@ -4,6 +4,7 @@ public class BranchRebase {
     public static void main(String[] args) {
         System.out.println("hellp");
         //ok lets try to practice git stash apply
+        // in the master I will try to show you how to stash our changes
 
     }
 }
