eb57ee28d5f26386ba2050cc6826a61af195388f
Baaber <tatykulov@gmail.com>
2023-06-23 13:54:33 -0500

    one command git commit -a

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         //jsut somme
+        // I think i will explain git rebase today
+        // can we do that
     }
 }
