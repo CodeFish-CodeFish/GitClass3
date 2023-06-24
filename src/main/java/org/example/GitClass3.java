d9cd3311280f1063f6eede29e6f2f20f1d2241da
codefish <contact@codefish.io>
2023-06-23 19:51:20 -0500

    somet

--- src/main/java/org/example/GitClass3.java
+++ src/main/java/org/example/GitClass3.java
@@ -3,6 +3,10 @@ package org.example;
 public class GitClass3 {
     public static void main(String[] args) {
         System.out.println("NewBranch");
+<<<<<<< Updated upstream
         //this is change from branch secondBranch
+=======
+        System.out.println("this change from master");
+>>>>>>> Stashed changes
     }
 }
