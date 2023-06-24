a2682b2ed1ac1fdee102b0117b039ca970587db5
codefish <contact@codefish.io>
2023-06-23 19:43:31 -0500

    secondBranch

--- src/main/java/org/example/GitClass3.java
+++ src/main/java/org/example/GitClass3.java
@@ -3,6 +3,6 @@ package org.example;
 public class GitClass3 {
     public static void main(String[] args) {
         System.out.println("NewBranch");
-
+        //this is change from branch secondBranch
     }
 }
